0d22f53050143379d022a197df572ce08bacda05
Julius Gonsior <julius@gaenseri.ch>
2016-08-25 17:45:54 +0200

    Setting up initial skeleton code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+
+    private static boolean isCorrect(String query) {
+        return false;
+    }
+
+    private static int countVariables(String query) {
+        int numberOfVariables = 0;
+        return numberOfVariables;
+    }
+
+    private static String removeComments(String query) {
+        return query;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String query = "";
+        System.out.println("Original Query: " + query);
+        System.out.println("Is the query correct? " + isCorrect(query));
+        System.out.println("How many variables does the query contain? " + countVariables(query));
+        System.out.println("Query string with all comments removed: " + removeComments(query));
     }
 }
