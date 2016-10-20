d7e3f76d23638c1cac393e1ca2c9f21ce611f515
Julius Gonsior <julius@gaenseri.ch>
2016-10-20 17:26:08 +0200

    Reformatted Adrians Code according to the style guide - needs to be done for all other files as well

--- src/main/java/metrics/CountVariables.java
+++ src/main/java/metrics/CountVariables.java
@@ -1,46 +1,52 @@
 package metrics;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 import org.apache.jena.graph.Node;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.sparql.core.TriplePath;
-import org.apache.jena.sparql.path.P_Link;
-import org.apache.jena.sparql.path.Path;
-import org.apache.jena.sparql.path.PathVisitorBase;
 import org.apache.jena.sparql.syntax.ElementPathBlock;
 import org.apache.jena.sparql.syntax.ElementVisitorBase;
 import org.apache.jena.sparql.syntax.ElementWalker;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * counts the number of variables in the query
+ */
 public class CountVariables implements Metric {
     @Override
-    public Object analyseQuery(String query_string) {
-		// Count the number of variables in the query
-		Query query;
-		try {
-			query = QueryFactory.create(query_string);
-		} catch (Exception e) {
-			throw e;
-		}
-		
-		Set<Node> variables  = new HashSet<Node>();
-		
-		ElementWalker.walk(query.getQueryPattern(), 
-				new ElementVisitorBase() {
-					public void visit(ElementPathBlock el) {
-						Iterator<TriplePath> triples = el.patternElts();
-						while (triples.hasNext()) {
-							TriplePath next = triples.next();
-							if (next.getSubject().isVariable()) variables.add(next.getSubject());
-							if (next.getPredicate() != null) {
-								if (next.getPredicate().isVariable()) variables.add(next.getPredicate());
-							};
-							if (next.getObject().isVariable()) variables.add(next.getObject());
-						}
-					}
-		});
+    public Object analyseQuery(String queryString) {
+        Query query;
+        try {
+            query = QueryFactory.create(queryString);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        Set<Node> variables = new HashSet<Node>();
+
+        ElementWalker.walk(query.getQueryPattern(),
+                new ElementVisitorBase() {
+                    public void visit(ElementPathBlock el) {
+                        Iterator<TriplePath> triples = el.patternElts();
+                        while (triples.hasNext()) {
+                            TriplePath next = triples.next();
+                            if (next.getSubject().isVariable()) {
+                                variables.add(next.getSubject());
+                            }
+                            if (next.getPredicate() != null) {
+                                if (next.getPredicate().isVariable()) {
+                                    variables.add(next.getPredicate());
+                                }
+                            }
+                            if (next.getObject().isVariable()) {
+                                variables.add(next.getObject());
+                            }
+                        }
+                    }
+                });
         return variables.size();
     }
 }
