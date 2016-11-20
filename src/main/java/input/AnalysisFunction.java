418cdab0a4cb9f2aaac77576dd22cd94128922a2
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2016-11-20 21:20:03 +0100

    Started InputHandlerParquet
    Started implementing a file reader for parquet. Does not yet work
    because it cannot be serialized.

---
+++ src/main/java/input/AnalysisFunction.java
@@ -0,0 +1,13 @@
+package input;
+
+import org.apache.spark.api.java.function.ForeachFunction;
+import org.apache.spark.sql.Row;
+
+public class AnalysisFunction implements ForeachFunction<Row>
+{
+  @Override
+  public void call(Row row) throws Exception
+  {
+  }
+
+}
