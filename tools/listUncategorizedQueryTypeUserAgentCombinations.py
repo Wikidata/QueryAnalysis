44934e40438e823e7c5a9b65dd171c1ca68b3213
Julius Gonsior <julius@gaenseri.ch>
2017-02-07 22:59:46 +0100

    Create directories for combinations

--- tools/listUncategorizedQueryTypeUserAgentCombinations.py
+++ tools/listUncategorizedQueryTypeUserAgentCombinations.py
@@ -15,7 +15,7 @@ files = []
 files.append("queryType/TotalQueryTypeCountSpider.tsv")
 files.append("queryType/TotalQueryTypeCountUser.tsv")
 
-topQueryTypes = set()
+queryTypeUserAgentCombinationsCount = dict()
 
 for file in files:
     print "Working on: " + file
@@ -26,11 +26,38 @@ for file in files:
                 continue
             if line_no > COMBINATIONS_LIMIT + 1:
                 break
-            topQueryTypes.add(line[0])
+            queryTypeUserAgentCombinationsCount[line[0]] = defaultdict(int)
 
-pprint(topQueryTypes)
 
 # grep QueryProcessedOpenRDFXX.tsv for these queryTypes and the respective userAgents and create a directory for
-
-
 # each of these found userAgents and put all found querys in it
+files = []
+for i in xrange(1, 2):
+    files.append("QueryProcessedOpenRDF" + "%02d" % i + ".tsv")
+
+
+for file in sorted(files):
+    print "Working on: " + file
+    with open(file) as f:
+        for line_no, line in enumerate(csv.reader(f, delimiter="\t")):
+            #skip header
+            if line_no == 0:
+                continue
+            #skip invalid ones
+            if int(line[0]) != 1:
+                continue
+
+            queryType = line[9]
+            userAgent = line[11]
+
+            if queryType in queryTypeUserAgentCombinationsCount.keys():
+                queryTypeUserAgentCombinationsCount[queryType][userAgent] += 1
+
+for queryType, userAgentCountDict in queryTypeUserAgentCombinationsCount.iteritems():
+    for userAgent, userAgentCount in userAgentCountDict.iteritems():
+
+        if not os.path.exists("queryTypeUserAgentCombinations/" + queryType + "/" + str(userAgentCount) + "_" + userAgent.replace('/', 'SLASH') ):
+            os.makedirs("queryTypeUserAgentCombinations/" + queryType + "/" + str(userAgentCount) + "_" + userAgent.replace('/', 'SLASH') )
+
+
+
