767b9d64bff466c1f1d3d2ac75007b01baa10cfb
Julius Gonsior <julius@gaenseri.ch>
2017-02-23 22:07:30 +0100

    Made plots useful

--- tools/plotClassifiedBotsData.py
+++ tools/plotClassifiedBotsData.py
@@ -5,12 +5,15 @@ from matplotlib.pyplot import cm
 import numpy as np
 import matplotlib.colors as mcolors
 from pprint import pprint
+import os
+import operator
 
 
-def plotHist(data, title, countTools):
-    plt.figure()
-    plt.subplot(111)
-    plt.subplots_adjust(right=0.7)
+def plotHist(data, title, countTools, log=False):
+    if not os.path.exists(title[:title.rfind("/")] ):
+        os.makedirs(title[:title.rfind("/")] )
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111)
     plt.grid(True)
 
     plt.title(title)
@@ -20,35 +23,53 @@ def plotHist(data, title, countTools):
     axes = plt.axes()
     axes.xaxis.set_major_locator(ticker.MultipleLocator(1))
 
-    #axes.set_yscale('log')
+    if log:
+        axes.set_yscale('log')
 
     color = iter(cm.rainbow(np.linspace(0, 1, countTools)))
 
-    for toolName, XY in data.iteritems():
+    for tool in data:
         c = next((color))
+
+        toolName = tool[0]
+        XY = tool[1]
+
         try:
-            plt.bar(XY["X"], XY["Y"], align='center', color=c, edgecolor=c, label=toolName + str(XY["Y"]))
+            ax.bar(XY["X"], XY["Y"], align='center', color=c, edgecolor=c, label=toolName + str(XY["Y"]))
         except ValueError:
             pass
 
-    plt.legend(bbox_to_anchor=(1, 1), loc='upper left', ncol=1, prop={'size':6})
+    handles, labels = ax.get_legend_handles_labels()
+    lgd = ax.legend(handles, labels, bbox_to_anchor=(1, 1), loc='upper left', ncol=1, prop={'size':6})
 
-
-    plt.xlim(0, 25)
+    plt.xlim(-1, 24)
     plt.xticks(fontsize=9)
-    plt.savefig("classifiedBotsData/" + title + ".png", dpi=120)
+    plt.savefig(title + ".png", bbox_extra_artists=(lgd,), bbox_inches='tight')
     plt.close()
 
 
 inputDirectory = "dayTriple/"
 
 files = []
-for i in xrange(1,2):
+for i in xrange(1,32):
     files.append("classifiedBotsData/" + "%02d"%i + "ClassifiedBotsData.tsv")
 files.append("classifiedBotsData/TotalClassifiedBotsData.tsv")
 
+def compare(item1, item2):
+    if max(item1[1]["Y"]) < max(item2[1]["Y"]):
+        return 1
+    elif max(item1[1]["Y"]) == max(item2[1]["Y"]):
+        return 0
+    else:
+        return -1
+
+
+
 for file in files:
     print "Working on: " + file
+
+    day = file.replace('classifiedBotsData/', '').replace("ClassifiedBotsData.tsv", '')
+
     with open(file) as f:
         reader = csv.DictReader(f, delimiter="\t")
 
@@ -57,6 +78,8 @@ for file in files:
         counts = []
 
         for line in reader:
+            if line['ToolName'] == '0':
+                continue
             hours.append(int(line['hour']))
             toolNames.append(line['ToolName'])
             counts.append(int(line['count']))
@@ -73,4 +96,8 @@ for file in files:
             data[toolName]["X"].append(hour)
             data[toolName]["Y"].append(count)
 
-        plotHist(data, file[file.rfind("/"):], len(toolNames))
+        #sort data so that the log graph is kind of useful
+        sorted_data = sorted(data.items(), cmp=compare)
+
+        plotHist(sorted_data, 'classifiedBotsData/plots/day' + day, len(set(toolNames)))
+        plotHist(sorted_data, 'classifiedBotsData/plots/log/day' + day, len(set(toolNames)), True)
--- tools/plotHourlyFieldValue.py
+++ tools/plotHourlyFieldValue.py
@@ -1,49 +1,62 @@
 import csv
 import os
-import csv
 from pprint import pprint
 
 import matplotlib.pyplot as plt
 
 
+def plotHist(X, Y, title, metric, log=False):
+    if not os.path.exists(title[:title.rfind("/")] ):
+        os.makedirs(title[:title.rfind("/")] )
 
-def plotHist(X, Y, title):
     plt.figure(figsize=(10, 6))
     plt.subplot(111)
     plt.grid(True)
 
     plt.title(title)
-    plt.xlabel("count of triples")
+    plt.xlabel(metric)
     plt.ylabel("count of queries")
 
     axes = plt.axes()
-    #axes.set_yscale('log')
+    if log:
+        axes.set_yscale('log')
+
+    axes.set_ylim([0,50000])
 
     plt.bar(X, Y, facecolor='#9999ff', edgecolor='#9999ff')
 
     plt.xlim(0)
-    #plt.show()
-    plt.savefig(title + ".png", dpi=100)
+    # plt.show()
+    plt.savefig(title + ".png")
     plt.close()
 
-metrics = ['StringLengthNoComments', 'VariableCountPattern']
+
+metrics = ['VariableCountPattern', 'StringLengthNoComments']
 for metric in metrics:
+
+    if not os.path.exists(metric + '/plots/log'):
+        os.makedirs(metric + '/plots/log')
 
     files = []
 
-    for i in xrange(1,2):
-        files.append(metric +  '/Day' + "%02d"%i + "Hourly" + metric + ".tsv")
-    files.append(metric +  '/TotalHourly' + metric + ".tsv")
+    for i in xrange(1, 32):
+        files.append(metric + '/Day' + "%02d" % i + "Hourly" + metric + ".tsv")
+    # files.append(metric +  '/TotalHourly' + metric + ".tsv")
 
     for file in files:
         print "Working on: " + file
+
+        day = file.replace(metric + '/Day', '').replace("Hourly" + metric + ".tsv", '')
+
         with open(file) as f:
             reader = csv.DictReader(f, delimiter="\t")
             for line in reader:
+
                 X = list(line.keys())
                 X.remove('hour')
                 Y = []
                 for x in X:
                     Y.append(int(line[x]))
-                if line['hour'] == '10':
-                    plotHist(X,Y, file +'Plot' + line['hour'])
+
+                plotHist(X,Y, metric + '/plots/day' + day + '/hour' + line['hour'], metric)
+                plotHist(X,Y, metric + '/plots/day'+ day +  '/log/hour' + line['hour'], metric, True)
