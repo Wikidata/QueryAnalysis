71fda8c3858d984ce02ba050ed19ef1f2ae3d74f
Julius Gonsior <julius@gaenseri.ch>
2017-04-21 15:46:24 +0200

    Parameterized getClassifiedBotsData.py

--- tools/getClassifiedBotsData.py
+++ tools/getClassifiedBotsData.py
@@ -12,6 +12,8 @@ import sys
 workingDir = sys.argv[1]
 os.chdir(workingDir)
 
+metricName = "ToolName"
+
 if not os.path.exists("classifiedBotsData"):
     os.makedirs("classifiedBotsData")
 
@@ -28,17 +30,17 @@ for i in xrange(1, 2):
             if line['#hour'] not in data:
                 data[line['#hour']] = defaultdict(int)
 
-            data[line['#hour']][line['#ToolName']] += 1
+            data[line['#hour']][line['#' + metricName]] += 1
 
-        header = "hour\tToolName\tcount\n"
+        header = "hour\t" + metricName + "\tcount\n"
         with open("classifiedBotsData/" + "%02d" % i + "ClassifiedBotsData.tsv", "w") as outputFile:
             outputFile.write(header)
-            for hour, toolNameDict in data.iteritems():
-                for toolName in toolNameDict.iterkeys():
-                    outputFile.write(str(hour) + "\t" + str(toolName) + "\t" + str(data[hour][toolName]) + "\n")
+            for hour, metricDict in data.iteritems():
+                for metric in metricDict.iterkeys():
+                    outputFile.write(str(hour) + "\t" + str(metric) + "\t" + str(data[hour][metric]) + "\n")
 
     with open("classifiedBotsData/TotalClassifiedBotsData.tsv", "w") as outputFile:
         outputFile.write(header)
-        for hour, toolNameDict in data.iteritems():
-            for toolName in toolNameDict.iterkeys():
-                outputFile.write(str(hour) + "\t" + str(toolName) + "\t" + str(data[hour][toolName]) + "\n")
+        for hour, metricDict in data.iteritems():
+            for metric in metricDict.iterkeys():
+                outputFile.write(str(hour) + "\t" + str(metric) + "\t" + str(data[hour][metric]) + "\n")
