5b2f2668fbb17dca66d0d6ea1171a3389fdeeedb
Markus Kroetzsch <markus@semantic-mediawiki.org>
2017-06-21 11:25:50 +0200

    new scripts

---
+++ tools/postprocess/processdata.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import csv
+#import getopt
+#import urllib
+import urlparse
+#import sys
+
+#from tabulate import tabulate
+#from itertools import izip
+
+processedPrefix = "QueryProcessedOpenRDF"
+sourcePrefix = "queryCnt"
+
+def processDay(day, startIdx, endIdx, handler):
+	with open(processedPrefix + "%02d" % day + ".tsv") as p, open(sourcePrefix + "%02d" % day + ".tsv") as s:
+		pReader = csv.DictReader(p, delimiter="\t")
+		sReader = csv.DictReader(s, delimiter="\t")
+
+		i = 0
+		for processed, source in izip(pReader, sReader):
+			if startIdx <= i <= endIdx:
+				requestParameters = dict(urlparse.parse_qsl(urlparse.urlsplit(source['uri_query']).query))
+				if 'query' in requestParameters.keys():
+					sparqlQuery = requestParameters['query']
+				else:
+					sparqlQuery = None
+					
+				handler.handle(sparqlQuery,processed)
+			elif i > endIdx:
+				break
+			i += 1
---
+++ tools/postprocess/showData.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import processdata as processdata
+
+#import csv
+import getopt
+#import urllib
+#import urlparse
+import sys
+
+#from tabulate import tabulate
+#from itertools import izip
+
+help = 'Usage: showData.py -d <day> -s <startline> -e <endline>'
+day = 0
+start = 0
+end = 0
+day_given = False
+start_given = False
+end_given = False
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hd:s:e:", ["day=", "startline=", "endline="])
+except getopt.GetoptError:
+	print help
+	sys.exit(2)
+for opt, arg in opts:
+	if opt == "-h":
+		print help
+		sys.exit()
+	elif opt in ("-d", "--day"):
+		day = int(arg)
+		day_given = True
+	elif opt in ("-s", "--startline"):
+		start = int(arg)
+		start_given = True
+	elif opt in ("-e", "--endline"):
+		end = int(arg)
+		end_given = True
+
+if not (day_given and start_given and end_given):
+	print help
+	sys.exit()
+
+
+class ViewDataHandler:
+	metrics = ["#QuerySize", "#TripleCountWithService"]
+	
+	def handle(self,sparqlQuery,processed):
+		data = [[]]
+		for metric in self.metrics:
+			data[0].append(processed[metric])
+		print tabulate(data, headers=metrics)
+		if sparqlQuery is None:
+			print "Error: Could not find query in uri_query."
+		else:
+			print sparqlQuery
+
+handler = ViewDataHandler()
+processdata.processDay(day, start, end, handler)
+
+#with open(processedPrefix + "%02d" % day + ".tsv") as p, open(sourcePrefix + "%02d" % day + ".tsv") as s:
+	#pReader = csv.DictReader(p, delimiter="\t")
+	#sReader = csv.DictReader(s, delimiter="\t")
+
+	#i = 0
+	#for processed, source in izip(pReader, sReader):
+		#if start <= i <= end:
+			#data = [[]]
+			#for metric in metrics:
+				#data[0].append(processed[metric])
+			#print tabulate(data, headers=metrics)
+			#d = dict(urlparse.parse_qsl(urlparse.urlsplit(source['uri_query']).query))
+			#if 'query' in d.keys():
+				#print d['query']
+			#else:
+				#print "Could not find query in uri_query."
+
+		#i += 1
+
