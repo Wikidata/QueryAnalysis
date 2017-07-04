743c543af2c3a280b26e25475a9701d349f9251f
Julius Gonsior <julius@gaenseri.ch>
2017-07-04 19:30:47 +0200

    Managed to convert tree creation code from javascript to python

--- tools/visualisePropertyTree/createHtml.py
+++ tools/visualisePropertyTree/createHtml.py
@@ -1,8 +1,8 @@
 import argparse
 import csv
-import json
+from pprint import pprint
 
-from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 parser = argparse.ArgumentParser(
 	description="Generates based on the content of the ranking.tsv file a JavaScript Json Object which contains detailed usage information about each property")
@@ -13,7 +13,18 @@ args = parser.parse_args()
 
 rankings = dict()
 
+print("Working on: " + args.rankingFile)
+with open(args.rankingFile, "r") as rankingFile:
+	rankingReader = csv.DictReader(rankingFile, delimiter="\t")
+
+	for ranking in rankingReader:
+		rankings["http://www.wikidata.org/entity/" + ranking["Categories"]] = (
+			ranking["Categories_count"], ranking["percentage"])
+
+# pprint(rankings)
+
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+
 sparql.setQuery("""
 #Tool: jgonsior-tree 
 SELECT ?property ?propertyLabel ?subclass0 ?subclass0Label ?subclass1 ?subclass1Label  ?subclass2 ?subclass2Label  ?subclass3 ?subclass3Label  ?subclass4 ?subclass4Label  ?subclass5 ?subclass5Label  ?subclass6 ?subclass6Label  ?subclass7 ?subclass7Label  ?subclass8 ?subclass8Label  ?subclass9 ?subclass9Label 
@@ -29,13 +40,47 @@ SELECT ?property ?propertyLabel ?subclass0 ?subclass0Label ?subclass1 ?subclass1
  OPTIONAL {?subclass9 wdt:P279 ?subclass8 .}}}}}}}}}} SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }}
 """)
 
-print("Working on: " + args.rankingFile)
-with open(args.rankingFile, "r") as rankingFile:
-	rankingReader = csv.DictReader(rankingFile, delimiter="\t")
+sparql.setReturnFormat(JSON)
+sparqlResult = sparql.query().convert()
 
-	for ranking in rankingReader:
-		rankings["http://www.wikidata.org/entity/" + ranking["Categories"]] = (
-			ranking["Categories_count"], ranking["percentage"])
 
-with open("ranking.json", "w") as rankingOutputFile:
-	json.dump(rankings, rankingOutputFile)
+def createProperty(name, qid):
+	property = dict()
+	property['name'] = name
+	property['qid'] = qid
+	property['children'] = list()
+	return property
+
+
+def searchPropertyInTree(treeRoot, qid):
+	'''$.each(parent.children, function (key, possibleChild) {
+	if (possibleChild.qid === value["subclass" + i].value) {
+		child = possibleChild;
+	}
+	});'''
+	for possibleProperty in treeRoot['children']:
+		if possibleProperty['qid'] == qid:
+			return possibleProperty
+	return None
+
+
+parentProperty = createProperty("/", "/")
+
+for property in sparqlResult['results']['bindings']:
+	parentProperty = searchPropertyInTree(parentProperty, property['property']['value'])
+	if parentProperty is None:
+		parentProperty = createProperty(property['propertyLabel']['value'], property['property']['value'])
+
+	parentProperty['children'].append(parentProperty)
+	for i in range(0, 9):
+		if 'subclass' + str(i) in property:
+
+			childProperty = searchPropertyInTree(parentProperty, property['subclass' + str(i)]['value'])
+			if childProperty is None:
+				childProperty = createProperty(property['subclass' + str(i) + "Label"]['value'],
+											   property['subclass' + str(i)]['value'])
+			parentProperty['children'].append(childProperty)
+			childProperty = parentProperty
+		else:
+			break
+pprint(parentProperty)
