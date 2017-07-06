3c31f782fd6bb12b24a897a9f2db13d2b640dc6c
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2017-07-06 13:46:29 +0200

    Updated hourlyFieldValue.py for new library

--- tools/hourlyFieldValue.py
+++ tools/hourlyFieldValue.py
@@ -1,79 +1,107 @@
+import argparse
 import csv
 import os
+import sys
 
 from itertools import izip
 
+from postprocess import processdata
+from utility import utility
 
-def writeOut(fieldValues, file, dictionary):
-	header = "hour"
-	for field in sorted(fieldValues):
-		header += "\t" + field
-	file.write(header + "\n")
-	for j in sorted(dictionary.keys()):
-		line = str(j)
+def writeOut(filename, fieldValues, dictionary):
+	with open(filename, "w") as file:
+		header = "hour"
 		for field in sorted(fieldValues):
-			if field in dictionary[j].keys():
-				line += "\t" + str(dictionary[j][field])
-			else:
-				line += "\t0"
-		file.write(line + "\n")
+			header += "\t" + field
+		file.write(header + "\n")
+		for j in sorted(dictionary.keys()):
+			line = str(j)
+			for field in sorted(fieldValues):
+				if field in dictionary[j].keys():
+					line += "\t" + str(dictionary[j][field])
+				else:
+					line += "\t0"
+			file.write(line + "\n")
 
+parser = argparse.ArgumentParser(
+	description="This script creates tables displaying all values of the specified field and their hourly count.")
+parser.add_argument("--monthsFolder", "-m", default="/a/akrausetud/months", type=str,
+                    help="The folder in which the months directory are residing.")
+parser.add_argument("--outputPath", "-o", type=str, help="The path where the output files should be generated.")
+parser.add_argument("metric", type=str, help="The metric that should be ranked")
+parser.add_argument("month", type=str, help="The month for which the ranking should be generated.")
 
-metrics = ['ToolName']
-for metric in metrics:
-	print "Working on " + metric
+if (len(sys.argv[1:]) == 0):
+	parser.print_help()
+	parser.exit()
 
-	pathBase = metric
+args = parser.parse_args()
 
-	if not os.path.exists(pathBase):
-		os.makedirs(pathBase)
+argMetric = args.metric
+metric = ""
 
+if argMetric.startswith("#"):
+	metric = argMetric[1:]
+else:
+	metric = argMetric
+
+pathBase = utility.addMissingSlash(args.monthsFolder) + utility.addMissingSlash(args.month) + utility.addMissingSlash(metric)
+
+if not os.path.exists(pathBase):
+	os.makedirs(pathBase)
+
+class hourlyFieldValueHandler:
 	monthlyFieldValues = set()
 
 	monthlyData = dict()
-
-	for i in xrange(1, 2):
-		print "\tWorking on %02d" % i
-		with open("QueryProcessedOpenRDF" + "%02d" % i + ".tsv") as p, open("queryCnt" + "%02d" % i + ".tsv") as s:
-			dailyFieldValues = set()
-
-			dailyData = dict()
+	
+	allDailyFieldValues = dict()
+	
+	allDailyData = dict()
+	
+	def handle(self, sparqlQuery, processed):
+		if processed['#Valid'] != "VALID":
+			return
+		
+		day = processed["#day"]
+		
+		if day not in self.allDailyFieldValues:
+			self.allDailyFieldValues[day] = set()
+			self.allDailyData[day] = dict()
 			for j in xrange(0, 24):
-				dailyData[j] = dict()
-				monthlyData[j + 24 * (i - 1)] = dict()
+				self.allDailyData[day][j] = dict()
+				self.monthlyData[j + 24 * (day - 1)] = dict()
 
-			pReader = csv.DictReader(p, delimiter="\t")
-			sReader = csv.DictReader(s, delimiter="\t")
-			for processed, source in izip(pReader, sReader):
-				if int(processed["#Valid"]) != 1:
-					continue
+		try:
+			hour = int(processed["#hour"])
+		except ValueError:
+			print processed["#hour"] + " could not be parsed as integer"
+			return
 
-				# onlfy for "user queries"
-				if processed['#ToolName'] == '0':
-					try:
-						hour = int(source["hour"])
-					except ValueError:
-						print source["hour"] + " could not be parsed as integer"
-						continue
+		if hour in self.allDailyData[day]:
+			data = processed["#" + metric]
+			self.allDailyFieldValues[day].add(data)
+			self.monthlyFieldValues.add(data)
+			if data in self.allDailyData[day][hour]:
+				self.allDailyData[day][hour][data] += 1
+			else:
+				self.allDailyData[day][hour][data] = 1
 
-					if hour in dailyData.keys():
-						data = processed["#" + pathBase]
-						dailyFieldValues.add(data)
-						monthlyFieldValues.add(data)
-						if data in dailyData[hour].keys():
-							dailyData[hour][data] += 1
-						else:
-							dailyData[hour][data] = 1
+			monthlyHour = hour + 24 * (day - 1)
+			if data in self.monthlyData[monthlyHour]:
+				self.monthlyData[monthlyHour][data] += 1
+			else:
+				self.monthlyData[monthlyHour][data] = 1
+		else:
+			print hour + " is not in 0-23"
+			
+	def writeHourlyValues(self):
+		writeOut(pathBase + "Full_Month_" + metric + "_Hourly_Values.tsv", self.monthlyFieldValues, self.monthlyData)
+		for day in self.allDailyFieldValues:
+			writeOut(pathBase + "Day_" + str(day) + "_" + metric + "_Hourly_Values.tsv", self.allDailyFieldValues[day], self.allDailyData[day])
+			
+handler = hourlyFieldValueHandler()
 
-						monthlyHour = hour + 24 * (i - 1)
-						if data in monthlyData[monthlyHour].keys():
-							monthlyData[monthlyHour][data] += 1
-						else:
-							monthlyData[monthlyHour][data] = 1
-					else:
-						print hour + " is not in 0-23"
-			with open(pathBase + "/" + "Day" + "%02d" % i + "Hourly" + pathBase + ".tsv", "w") as dailyfile:
-				writeOut(dailyFieldValues, dailyfile, dailyData)
+processdata.processMonth(handler, args.month, args.monthsFolder)
 
-	with open(pathBase + "/" + "TotalHourly" + pathBase + ".tsv", "w") as monthlyfile:
-		writeOut(monthlyFieldValues, monthlyfile, monthlyData)
+handler.writeHourlyValues()
