b214100cc4955c8a7545f48ce1582270b39d20c4
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2017-07-13 19:06:58 +0200

    Finished QueryAnalysis.py

--- tools/QueryAnalysis.py
+++ tools/QueryAnalysis.py
@@ -8,16 +8,29 @@ import shutil
 import subprocess
 import sys
 
-from utility import utility
 from datetime import datetime
+
+import unifyQueryTypes
+
+from utility import utility
 
 months = {'january': [1,31], 'february': [2,28], 'march': [3,31], 'april': [4,30], 'may': [5,31], 'june': [6,30], 'july': [7,30], 'august': [8,31], 'september': [9,30], 'october': [10,31], 'november': [11,30], 'december': [12,31]}
 
 parser = argparse.ArgumentParser("This script extracts the raw log data (if it was not already done), processes them using the java application and unifies the query types.")
+parser.add_argument("--ignoreLock", "-i", help="Ignore locked file and execute anyways", action="store_true")
+parser.add_argument("--threads", "-t", default=5, type=int, help="The number of threads to run the java program with (default 5).")
+parser.add_argument("--logging", "-l", help="Enables file logging.", action="store_true")
+parser.add_argument("--noBotMetrics", "-b", help="Disables metric calculation for bot queries.", action="store_true")
+parser.add_argument("--noDynamicQueryTypes", "-d", help="Disables dynamic generation of query types.", action="store_true")
+parser.add_argument("--noGzipOutput", "-g", help="Disables gzipping of the output files.", action="store_true")
+parser.add_argument("--noExampleQueriesOutput", "-e", help="Disables the matching of example queries.", action="store_true")
+parser.add_argument("--referenceDirectory", "-r", default=unifyQueryTypes.defaultReferenceDirectory, type=str,
+                    help="The directory with the reference query types.")
+parser.add_argument("--fdupesExecutable", "-f", default=unifyQueryTypes.defaultFdupesExecutable, type=str,
+                    help="The location of the fdupes executable.")
 parser.add_argument("--monthsFolder", "-m", default="/a/akrausetud/months", type=str,
                     help="The folder in which the months directory are residing.")
-parser.add_argument("--ignoreLock", "-i", help="Ignore locked file and execute anyways", action="store_true")
-parser.add_argument("--year", "-y", default=datetime.now().year, type=int, help="The year to be processed (default current year)")
+parser.add_argument("--year", "-y", default=datetime.now().year, type=int, help="The year to be processed (default current year).")
 parser.add_argument("month", type=str, help="The month to be processed")
 
 # These are the field we extract from wmf.wdqs_extract that form the raw log data.
@@ -42,7 +55,7 @@ if os.path.isfile(utility.addMissingSlash(args.monthsFolder) + utility.addMissin
     print "ERROR: The month " + args.month + " is being edited at the moment. Use -i if you want to force the execution of this script."
     sys.exit()
     
-month = utility.addMissingSlash(args.monthsFolder) + utility.addMissingSlash(args.month)
+month = os.path.abspath(utility.addMissingSlash(args.monthsFolder) + utility.addMissingSlash(args.month))
 
 processedLogDataDirectory = month + "processedLogData/"
 rawLogDataDirectory = month + "rawLogData/"
@@ -50,6 +63,9 @@ tempDirectory = rawLogDataDirectory + "temp/"
 
 # If the month directory does not exist it is being created along with the directories for raw and processed log data.
 if not os.path.exists(month):
+    
+    print "Starting data extraction from wmf.wdqs_extract for " + args.month + "."
+    
     os.makedirs(month)
     os.makedirs(processedLogDataDirectory)
     os.makedirs(rawLogDataDirectory)
@@ -60,8 +76,11 @@ if not os.path.exists(month):
     for day in xrange(1, months[args.month][1] + 1):
         os.makedirs(tempDirectory)
         hive_call = 'insert overwrite local directory \'' + tempDirectory + '\' row format delimited fields terminated by \'\\t\' select '
+        
+        # We add all the fields to the request
         for field in fields:
             hive_call += field + " "
+            
         hive_call += ' from wmf.wdqs_extract where uri_query<>"" and year=\'' + str(args.year) +  '\' and month=\'' + months[args.month][0] + '\' and day=\'' + day + '\''
         
         arguments.append(hive_call)
@@ -79,3 +98,39 @@ if not os.path.exists(month):
                         dayfile.write(line)
     
         shutil.rmtree(tempDirectory)
+
+# We build the call to execute the java application with the location of the files, the number of threads to use and any optional arguments needed
+
+mavenCall = ['mvn', 'exec:java']    
+
+mavenArguments = '-Dexec.args=-w ' + month + ' -n ' + str(args.threads)
+
+if args.logging:
+    mavenArguments += " -l"
+if args.noBotMetrics:
+    mavenArguments += " -b"
+if args.noDynamicQueryTypes:
+    mavenArguments += " -d"
+if args.noGzipOutput:
+    mavenArguments += " -g"
+if args.noExampleQueriesOutput:
+    mavenArguments += " -e"
+
+mavenCall.append(mavenArguments)
+
+owd = os.getcwd()
+os.chdir("..")
+
+print "Starting data processing using QueryAnalysis for " + args.month + "."  
+    
+if subprocess.call(mavenCall) != 0:
+    print "ERROR: Could not execute the java application. Check the logs for details or rerun this script with -l to generate logs."
+    sys.exit(1)
+    
+os.chdir(owd)
+
+# Finally we call the query type unification (see python unifyQueryTypes.py for details)
+
+print "Starting to unify the query types for " + args.month + "."
+
+unifyQueryTypes.unifyQueryTypes(args.month, args.monthsFolder, args.referenceDirectory, args.fdupesExecutable)
