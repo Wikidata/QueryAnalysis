ca875a4e9f0ee01255510d2f9c96991f939389ca
Julius Gonsior <julius@gaenseri.ch>
2017-08-03 13:00:44 +0000

    Added config.py

--- tools/countTools.py
+++ tools/countTools.py
@@ -3,38 +3,45 @@ import os
 import pprint
 import sys
 from collections import defaultdict
-
-import sys
-
 from postprocess import processdata
 from utility import utility
+from config import *
 
-parser = argparse.ArgumentParser(description="Counts the used tools/bots in the given folder")
-parser.add_argument("--monthsFolder", "-m", default="/a/akrausetud/months", type=str,
-					help="the folder in which the months directory are residing")
-parser.add_argument("--ignoreLock", "-i", help="Ignore locked file and execute anyways", action="store_true")
-parser.add_argument("month", type=str, help="the month which we're interested in")
+parser = argparse.ArgumentParser(
+    description="Counts the used tools/bots in the given folder")
+parser.add_argument("--monthsFolder", "-m", default=monthsFolder,
+                    type=str, help="the folder in which the months"
+                    + " directory are residing")
+parser.add_argument("--ignoreLock", "-i",
+                    help="Ignore locked file and execute anyways",
+                    action="store_true")
+parser.add_argument("month", type=str,
+                    help="the month which we're interested in")
 
 if (len(sys.argv[1:]) == 0):
-	parser.print_help()
-	parser.exit()
+    parser.print_help()
+    parser.exit()
 
 args = parser.parse_args()
 
-if os.path.isfile(utility.addMissingSlash(args.monthsFolder) + utility.addMissingSlash(args.month) + "locked") and not args.ignoreLock:
-	print "ERROR: The month " + args.month + " is being edited at the moment. Use -i if you want to force the execution of this script."
-	sys.exit()
+if os.path.isfile(utility.addMissingSlash(args.monthsFolder) +
+                  utility.addMissingSlash(args.month) + "locked") \
+   and not args.ignoreLock:
+    print "ERROR: The month " + args.month + " is being edited at the moment."
+    + " Use -i if you want to force the execution of this script."
+    sys.exit()
 
 
 class CountToolsHandler:
-	toolCounter = defaultdict(int)
+    toolCounter = defaultdict(int)
 
-	def handle(self, sparqlQuery, processed):
-		if (processed['#Valid'] == 'VALID' or processed['#Valid'] == '1'):
-			self.toolCounter[processed['#ToolName']] += 1
+    def handle(self, sparqlQuery, processed):
+        if (processed['#Valid'] == 'VALID' or processed['#Valid'] == '1'):
+            self.toolCounter[processed['#ToolName']] += 1
 
-	def __str__(self):
-		return pprint.pformat(sorted(self.toolCounter.iteritems(), key=lambda x: x[1], reverse=True))
+    def __str__(self):
+        return pprint.pformat(sorted(self.toolCounter.iteritems(),
+                                     key=lambda x: x[1], reverse=True))
 
 
 handler = CountToolsHandler()
