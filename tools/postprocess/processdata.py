5a6c823ce72bd711543c34004eabcff8314ce654
Julius Gonsior <julius@gaenseri.ch>
2017-08-03 14:58:56 +0000

    Postprocess can now work with gunzipped rawLogFiles

--- tools/postprocess/processdata.py
+++ tools/postprocess/processdata.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import csv
 import glob
 import gzip
@@ -18,37 +17,42 @@ sourcePrefix = "queryCnt"
 
 # iterates over all processed files in the given folder
 def processMonth(handler, month, monthsFolder):
-	for filename in glob.glob(monthsFolder + "/" + month
-			                          + "/processedLogData/" + processedPrefix + "*" + processedSuffix):
-		day = os.path.basename(filename)[len(processedPrefix):][:-len(processedSuffix)]
-		processDay(handler, int(day), month, monthsFolder)
+    for filename in glob.glob(monthsFolder + "/" + month
+                              + "/processedLogData/" + processedPrefix + "*"
+                              + processedSuffix):
+        day = os.path.basename(filename)[len(processedPrefix):][:-len(processedSuffix)]
+        processDay(handler, int(day), month, monthsFolder)
 
 
 ''' monthsFolder is only needed for development
 '''
 
 
-def processDay(handler, day, month, monthsFolder, startIdx=0, endIdx=sys.maxint):
-	processedFileName = utility.addMissingSlash(monthsFolder) + month + "/processedLogData/" + processedPrefix + "%02d" % day + processedSuffix
+def processDay(handler, day, month, monthsFolder,
+               startIdx=0, endIdx=sys.maxint):
+    processedFileName = utility.addMissingSlash(monthsFolder) + month \
+    + "/processedLogData/" + processedPrefix + "%02d" % day + processedSuffix
 
-	print "Working on: " + processedFileName
-	with gzip.open(processedFileName) as p, open(monthsFolder + "/" + month
-			                                             + "/rawLogData/" + sourcePrefix + "%02d" % day + ".tsv") as s:
-		pReader = csv.DictReader(p, delimiter="\t")
-		sReader = csv.DictReader(s, delimiter="\t")
+    print "Working on: " + processedFileName
+    with gzip.open(processedFileName) as p, \
+            gzip.open(monthsFolder + "/" + month + "/rawLogData/"
+                      + sourcePrefix + "%02d" % day + ".tsv.gz") as s:
+        pReader = csv.DictReader(p, delimiter="\t")
+        sReader = csv.DictReader(s, delimiter="\t")
 
-		i = 0
-		for processed, source in izip(pReader, sReader):
-			if startIdx <= i <= endIdx:
-				requestParameters = dict(urlparse.parse_qsl(urlparse.urlsplit(source['uri_query']).query))
-				if 'query' in requestParameters.keys():
-					sparqlQuery = requestParameters['query']
-				else:
-					sparqlQuery = None
-				processed['#hour'] = source['hour']
-				processed['#day'] = day
-				processed['#user_agent'] = source['user_agent']
-				handler.handle(sparqlQuery, processed)
-			elif i > endIdx:
-				break
-			i += 1
+        i = 0
+        for processed, source in izip(pReader, sReader):
+            if startIdx <= i <= endIdx:
+                requestParameters = dict(urlparse.parse_qsl(urlparse.urlsplit(
+                    source['uri_query']).query))
+                if 'query' in requestParameters.keys():
+                    sparqlQuery = requestParameters['query']
+                else:
+                    sparqlQuery = None
+                    processed['#hour'] = source['hour']
+                    processed['#day'] = day
+                    processed['#user_agent'] = source['user_agent']
+                    handler.handle(sparqlQuery, processed)
+            elif i > endIdx:
+                break
+            i += 1
