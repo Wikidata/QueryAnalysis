d5844336759364595c0e6b075fe168e0c32fcdee
Julius Gonsior <julius@gaenseri.ch>
2017-08-04 08:47:06 +0000

    Add createTestData.py

--- tools/createTestData.py
+++ tools/createTestData.py
@@ -4,12 +4,10 @@ import sys
 from postprocess import processdata
 from utility import utility
 import config
-import csv
 import glob
 import gzip
 from itertools import izip
 from random import random
-from pprint import pprint
 
 parser = argparse.ArgumentParser(
     description="Creates a smaller testdata set for developing of the"
@@ -22,6 +20,8 @@ parser.add_argument("--ignoreLock", "-i",
                     action="store_true")
 parser.add_argument("month", type=str,
                     help="the month which we're interested in")
+parser.add_argument("lines", type=int,
+                    help="number of lines the testfiles should have")
 
 if (len(sys.argv[1:]) == 0):
     parser.print_help()
@@ -40,32 +40,43 @@ if os.path.isfile(utility.addMissingSlash(monthsFolder) +
     sys.exit()
 
 # create new folder for the test data
-os.makedirs(monthsFolder + "/testData")
+os.makedirs("testData/processedLogData")
+os.makedirs("testData/rawLogData")
 
 for filename in glob.glob(monthsFolder + "/" + month
                           + "/processedLogData/" + processdata.processedPrefix
                           + "*" + processdata.processedSuffix):
-    day = os.path.basename(filename)[len(processdata.processedPrefix):]\
-            [:-len(processdata.processedSuffix)]
+    day = int(os.path.basename(filename)[len(processdata.processedPrefix):]
+              [:-len(processdata.processedSuffix)])
     processedFileName = utility.addMissingSlash(monthsFolder) + month \
             + "/processedLogData/" + processdata.processedPrefix \
             + "%02d" % day + processdata.processedSuffix
     print "Working on: " + processedFileName
+
     with gzip.open(processedFileName) as p, \
-            gzip.open(monthsFolder + "/" + month + "/rawLogData/"
-                      + processdata.sourcePrefix
-                      + "%02d" % day + ".tsv.gz") as s,\
-            gzip.open(monthsFolder + "/testData/processedLogData/" +
-                      processdata.processedPrefix + "%02d" % day +
-                      processedFileName) as pc,\
-            gzip.open(monthsFolder + "/testData/rawLogData/" +
+            gzip.open("testData/processedLogData/" +
+                      processdata.processedPrefix + "%02d" % day
+                      + ".tsv.gz", "wb") as pc,\
+            gzip.open("testData/rawLogData/" +
                       processdata.sourcePrefix +
-                      "%02d" % day + ".tsv.gz") as sc:
+                      "%02d" % day + ".tsv.gz", "wb") as sc:
 
-        chancesSelected = 1000 / sum(1 for line in p)
+        chancesSelected = float(args.lines) / float(sum(1 for line in p))
 
-        for processed, source in izip(p, s):
-            if(random() > chancesSelected):
+        print "Done counting lines, chance for selection is " \
+                + str(chancesSelected)
+
+        headerRow = True
+
+        for processed, source in izip(gzip.open(processedFileName),
+                                      gzip.open(monthsFolder + "/" + month +
+                                                "/rawLogData/" +
+                                                processdata.sourcePrefix +
+                                                "%02d" % day + ".tsv.gz")):
+            if(random() >= chancesSelected and not headerRow):
                 continue
-            pc.write(p)
-            sc.write(s)
+            elif headerRow:
+                headerRow = False
+
+            pc.write(processed)
+            sc.write(source)
