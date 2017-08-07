7e7eac035853e3a5a887c83b0c8ee753fa543a99
Julius Gonsior <julius@gaenseri.ch>
2017-08-07 15:08:56 +0000

    Removed old gui

--- tools/geoHeatMap.py
+++ tools/geoHeatMap.py
@@ -2,21 +2,20 @@ import argparse
 import os
 from pprint import pprint
 import sys
-from collections import defaultdict
 import os.path
-import sys
-
-import cartopy.crs as ccrs 
-import matplotlib.pyplot as plt
-
 from postprocess import processdata
 from utility import utility
+import config
 
-parser = argparse.ArgumentParser(description="Generaets a heatmap based on geo coordinates")
-parser.add_argument("--monthsFolder", "-m", default="/a/akrausetud/months", type=str,
-                    help="the folder in which the months directory are residing")
-parser.add_argument("--ignoreLock", "-i", help="Ignore locked file and execute anyways", action="store_true")
-parser.add_argument("month", type=str, help="the month which we're interested in")
+parser = argparse.ArgumentParser(description="Generates a heatmap based on "
+                                 + "geo coordinates")
+parser.add_argument("--monthsFolder", "-m", default=config.monthsFolder,
+                    type=str, help="the folder in which the months directory"
+                    + " are residing")
+parser.add_argument("--ignoreLock", "-i", help="Ignore locked file and "
+                    + "execute anyways", action="store_true")
+parser.add_argument("month", type=str,
+                    help="the month which we're interested in")
 
 if (len(sys.argv[1:]) == 0):
     parser.print_help()
@@ -24,9 +23,16 @@ if (len(sys.argv[1:]) == 0):
 
 args = parser.parse_args()
 
-if os.path.isfile(utility.addMissingSlash(args.monthsFolder) + utility.addMissingSlash(args.month) + "locked") and not args.ignoreLock:
-    print "ERROR: The month " + args.month + " is being edited at the moment. Use -i if you want to force the execution of this script."
+if os.path.isfile(utility.addMissingSlash(args.monthsFolder)
+                  + utility.addMissingSlash(args.month) + "locked") \
+   and not args.ignoreLock:
+    print "ERROR: The month " + args.month + " is being edited at the moment."
+    + " Use -i if you want to force the execution of this script."
     sys.exit()
+
+# user, bot, unknown
+# date
+
 
 class GeoCoordinateCollectorHandler:
     coordinates = set()
@@ -36,34 +42,15 @@ class GeoCoordinateCollectorHandler:
             if(processed['#Coordinates'] is not ''):
                 for coordinate in processed['#Coordinates'].split(","):
                     self.coordinates.add(coordinate)
-    
-    def saveSetToJson(self):
+
+    def saveSetToFile(self):
         pprint(self.coordinates)
         with open('geoCoordinates.tsv', 'w') as geoCoordinatesFile:
             for coordinate in self.coordinates:
                 geoCoordinatesFile.write(coordinate.replace(" ", "\t") + "\n")
 
 
-if not os.path.isfile('geoCoordinates.tsv'):
-    # first get all geo coordinates and save them to a file
-    handler = GeoCoordinateCollectorHandler()
-    processdata.processMonth(handler, args.month, args.monthsFolder)
-    handler.saveSetToJson()
-
-else:
-    # parse geoCoordinates.tsv and create choropleth map
-    
-    
-    ax = plt.axes(projection=ccrs.PlateCarree()) 
-    ax.coastlines()
-    ax.stock_img()
-
-
-
-    with open('geoCoordinates.tsv', 'r') as file:
-        for line in file:
-            lat,lon = line.strip('\n').split(" ")
-            plt.plot(float(lat), float(lon), color='red', alpha=.3, marker='.',transform=ccrs.PlateCarree())
-            #pprint(line.split(" "))
-    
-    plt.show()
+# first get all geo coordinates and save them to a file
+handler = GeoCoordinateCollectorHandler()
+processdata.processMonth(handler, args.month, args.monthsFolder)
+handler.saveSetToFile()
