c0f7b1cdf87e00b0d541a15b42af95ee601d5a62
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2017-10-15 15:00:44 +0200

    Revert "Altered extractUniqueDataset to generate user and non-user sets"
    This reverts commit e3b0baf34b91e4ac1461202dd05c4fc27d8e6c6b.

--- tools/extractUniqueDataset.py
+++ tools/extractUniqueDataset.py
@@ -42,97 +42,44 @@ os.chdir(
     utility.addMissingSlash(args.monthsFolder) +
     utility.addMissingSlash(args.month))
 
-subfolderUser = "uniqueQueryDatasetUser/"
-subfolderNonUser = "uniqueQueryDatasetNonUser/"
+subfolder = "uniqueQueryDataset/"
 
 processedPrefix = "processedLogData/QueryProcessedOpenRDF"
 sourcePrefix = "rawLogData/QueryCnt"
 
-if not os.path.exists(subfolderUser):
-    os.makedirs(subfolderUser)
-    os.makedirs(subfolderUser + "processedLogData")
-    os.makedirs(subfolderUser + "rawLogData")
+if not os.path.exists(subfolder):
+    os.makedirs(subfolder)
+    os.makedirs(subfolder + "processedLogData")
+    os.makedirs(subfolder + "rawLogData")
 
-if not os.path.exists(subfolderNonUser):
-    os.makedirs(subfolderNonUser)
-    os.makedirs(subfolderNonUser + "processedLogData")
-    os.makedirs(subfolderNonUser + "rawLogData")
+for i in xrange(1, 32):
+    if not (os.path.exists(processedPrefix + "%02d" % i + ".tsv.gz")
+            and gzip.os.path.exists(sourcePrefix + "%02d" % i + ".tsv.gz")):
+        continue
+    print "Working on %02d" % i
+    with gzip.open(processedPrefix + "%02d" % i + ".tsv.gz") as p, \
+            gzip.open(sourcePrefix + "%02d" % i + ".tsv.gz") as s, \
+            gzip.open(subfolder + processedPrefix + "%02d" % i
+                      + ".tsv.gz", "w") as user_p, \
+            gzip.open(subfolder + sourcePrefix + "%02d" % i
+                      + ".tsv.gz", "w") as user_s:
+        pReader = csv.DictReader(p, delimiter="\t")
+        sReader = csv.DictReader(s, delimiter="\t")
 
-userToFind = list()
-nonUserToFind = list()
+        pWriter = csv.DictWriter(user_p, None, delimiter="\t")
+        sWriter = csv.DictWriter(user_s, None, delimiter="\t")
 
-firstRun = True
+        for processed, source in izip(pReader, sReader):
+            if pWriter.fieldnames is None:
+                ph = dict((h, h) for h in pReader.fieldnames)
+                pWriter.fieldnames = pReader.fieldnames
+                pWriter.writerow(ph)
 
-for value in xrange(0,2):
-    if ((not firstRun) and (len(userToFind) == 0) and (len(nonUserToFind) == 0)):
-        break
+            if sWriter.fieldnames is None:
+                sh = dict((h, h) for h in sReader.fieldnames)
+                sWriter.fieldnames = sReader.fieldnames
+                sWriter.writerow(sh)
 
-    for i in xrange(1, 32):
-        if not (os.path.exists(processedPrefix + "%02d" % i + ".tsv.gz")
-                and gzip.os.path.exists(sourcePrefix + "%02d" % i + ".tsv.gz")):
-            continue
-        print "Working on %02d" % i
-        if firstRun:
-            parameter = "w"
-        else:
-            parameter = "a"
-        with gzip.open(processedPrefix + "%02d" % i + ".tsv.gz") as p, \
-                gzip.open(sourcePrefix + "%02d" % i + ".tsv.gz") as s, \
-                gzip.open(subfolderUser + processedPrefix + "%02d" % i
-                          + ".tsv.gz", parameter) as user_p, \
-                gzip.open(subfolderUser + sourcePrefix + "%02d" % i
-                          + ".tsv.gz", parameter) as user_s, \
-    			gzip.open(subfolderNonUser + processedPrefix + "%02d" % i
-                          + ".tsv.gz", parameter) as non_user_p, \
-                gzip.open(subfolderNonUser + sourcePrefix + "%02d" % i
-                          + ".tsv.gz", parameter) as non_user_s:
-            pReader = csv.DictReader(p, delimiter="\t")
-            sReader = csv.DictReader(s, delimiter="\t")
-
-            pUserWriter = csv.DictWriter(user_p, None, delimiter="\t")
-            sUserWriter = csv.DictWriter(user_s, None, delimiter="\t")
-
-            pNonUserWriter = csv.DictWriter(non_user_p, None, delimiter="\t")
-            sNonUserWriter = csv.DictWriter(non_user_s, None, delimiter="\t")
-
-            ph = dict((h, h) for h in pReader.fieldnames)
-            pUserWriter.fieldnames = pReader.fieldnames
-
-            sh = dict((h, h) for h in sReader.fieldnames)
-            sUserWriter.fieldnames = sReader.fieldnames
-
-            ph = dict((h, h) for h in pReader.fieldnames)
-            pNonUserWriter.fieldnames = pReader.fieldnames
-
-            sh = dict((h, h) for h in sReader.fieldnames)
-            sNonUserWriter.fieldnames = sReader.fieldnames
-
-            if firstRun:
-                pUserWriter.writerow(ph)
-                sUserWriter.writerow(sh)
-
-                pNonUserWriter.writerow(ph)
-                sNonUserWriter.writerow(sh)
-
-            for processed, source in izip(pReader, sReader):
-                originalId = processed['#OriginalId']
-
-                if (processed['#SourceCategory'] == "USER"):
-                    if (originalId in userToFind):
-                        userToFind.remove(originalId)
-                        pUserWriter.writerow(processed)
-                        sUserWriter.writerow(source)
-                    elif (processed['#First'] == "FIRST" and firstRun):
-                        nonUserToFind.append(processed['#UniqueId'])
-                        pUserWriter.writerow(processed)
-                        sUserWriter.writerow(source)
-                else:
-                    if (originalId in nonUserToFind):
-                        nonUserToFind.remove(originalId)
-                        pNonUserWriter.writerow(processed)
-                        sNonUserWriter.writerow(source)
-                    elif (processed['#First'] == "FIRST" and firstRun):
-                        userToFind.append(processed['#UniqueId'])
-                        pNonUserWriter.writerow(processed)
-                        sNonUserWriter.writerow(source)
-    firstRun = False
+            if (processed['#First'] == "FIRST"):
+                pWriter.writerow(processed)
+                sWriter.writerow(source)
