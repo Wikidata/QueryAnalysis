d57681f98ad4f2582791d4a76d830b1502c29806
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2017-10-23 13:42:19 +0200

    FIX: Better progress output for getDriveStatistics.py

--- tools/getDriveStatistics.py
+++ tools/getDriveStatistics.py
@@ -44,11 +44,12 @@ for monthName in args.months.split(","):
             monthFolder = monthFolder.strip("/")
 
             filename = cleanMonthName + "#" + secondKey + "#" + thirdKey
-            print "Working with fieldRanking on " + filename
+            print "Working with fieldRanking Predicates on " + filename
             fieldRanking.fieldRanking(monthFolder, "Predicates", monthsFolder = args.monthsFolder, outputPath = statisticsSubfolder + "predicates_Ranking", outputFilename = filename, writeOut = True, notifications = False)
-            print "Working with fieldRanking on " + filename
+            print "Working with fieldRanking Categories on " + filename
             fieldRanking.fieldRanking(monthFolder, "Categories", monthsFolder = args.monthsFolder, outputPath = statisticsSubfolder + "categories_Ranking", outputFilename = filename, writeOut = True, notifications = False)
             if thirdKey is not "queryType":
+                print "Working with fieldRanking QueryType on " + filename
                 fieldRanking.fieldRanking(monthFolder, "QueryType", monthsFolder = args.monthsFolder, outputPath = statisticsSubfolder + "queryType_Ranking", outputFilename = filename, writeOut = True, notifications = False)
             for script, scriptFolder in {"getSparqlStatistic.py":"sparqlFeatures", "operatorUsageStatistic.py":"operatorUsage", "generalStat.py":"generalStats"}.iteritems():
                 folder = utility.addMissingSlash(statisticsSubfolder + scriptFolder)
