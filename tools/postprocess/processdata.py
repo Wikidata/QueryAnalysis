eaa2f1cd7deb1b1ece0e8d33834bcf41ec58188e
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2018-04-15 18:51:44 +0200

    Added ts to postprocess

--- tools/postprocess/processdata.py
+++ tools/postprocess/processdata.py
@@ -76,6 +76,7 @@ def processDay(handler, day, month, monthsFolder,
                 processed['#day'] = day
                 processed['#user_agent'] = source['user_agent']
                 processed['#http_status'] = source['http_status']
+                processed['#ts'] = source['ts']
                 handler.handle(sparqlQuery, processed)
             elif i > endIdx:
                 break
