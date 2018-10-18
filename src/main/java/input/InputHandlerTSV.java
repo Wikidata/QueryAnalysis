b1d67329e8a3cafcdc96003532257669d6062d67
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2018-10-18 19:30:13 +0200

    Switched input to apache commons csv

--- src/main/java/input/InputHandlerTSV.java
+++ src/main/java/input/InputHandlerTSV.java
@@ -1,17 +1,19 @@
 package input;
 
-import com.univocity.parsers.common.ParsingContext;
-import com.univocity.parsers.common.processor.ObjectRowProcessor;
-import com.univocity.parsers.tsv.TsvParser;
-import com.univocity.parsers.tsv.TsvParserSettings;
-import com.univocity.parsers.tsv.TsvWriter;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.log4j.Logger;
 import output.OutputHandler;
 import query.QueryHandler;
 import scala.Tuple2;
-
-import java.io.*;
-import java.util.zip.GZIPInputStream;
 
 /**
  * @author adrian
@@ -30,14 +32,13 @@ public class InputHandlerTSV extends InputHandler
   private Reader reader;
 
   /**
-   * The file for writing the pre-processed data.
+   * The parser to read the .tsv-Files.
    */
-  private TsvWriter preprocessedWriter;
+  private CSVParser csvParser;
 
   /**
    * @param fileToRead The file to read.
-   * @throws FileNotFoundException If the file does not exist.
-   * @throws IOException           If another error occured.
+   * @throws IOException           If another error occurred.
    */
   public InputHandlerTSV(String fileToRead) throws IOException
   {
@@ -46,73 +47,53 @@ public class InputHandlerTSV extends InputHandler
 
   /**
    * @param fileToRead The file the parse()-method should read from.
-   * @throws IOException           if an I/O error has occurred
-   * @throws FileNotFoundException If the file does not exist,
-   *                               is a directory rather than a regular file,
-   *                               or for some other reason cannot be opened for reading.
+   * @throws IOException           if an I/O error has occurredd
    */
   public void setInputFile(String fileToRead) throws IOException
   {
     this.inputFile = fileToRead;
-    this.reader = new InputStreamReader(new GZIPInputStream(new FileInputStream(fileToRead)));
+    this.reader = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fileToRead))));
+    csvParser = new CSVParser(reader,
+        CSVFormat.newFormat('\t')
+        .withFirstRecordAsHeader()
+        .withIgnoreHeaderCase()
+        .withIgnoreEmptyLines()
+        .withTrim());
   }
 
   /**
    * Read the file given by reader and hands the data to the outputHandler.
-   *
+   * @param day The day currently being processed.
    * @param outputHandler Handles the data that should be written.
    */
   public final void parseTo(final OutputHandler outputHandler, int day)
   {
-    //read in queries from .tsv
-    TsvParserSettings parserSettings = new TsvParserSettings();
-    parserSettings.setLineSeparatorDetectionEnabled(true);
-    parserSettings.setHeaderExtractionEnabled(true);
-    parserSettings.setSkipEmptyLines(true);
-    parserSettings.setReadInputOnSeparateThread(true);
-
-    ObjectRowProcessor rowProcessor = new ObjectRowProcessor()
-    {
-      @Override
-      public void rowProcessed(Object[] row, ParsingContext parsingContext)
-      {
-        if (row.length <= 1) {
-          logger.warn("Ignoring line without tab while parsing.");
-          return;
-        }
-        Tuple2<String, QueryHandler.Validity> queryTuple = decode(row[0].toString(), inputFile, parsingContext.currentLine());
-
-        String queryString = queryTuple._1;
-        QueryHandler.Validity validity = queryTuple._2;
-        String userAgent = "null";
-        if (row[2] != null) {
-          userAgent = row[2].toString();
-        }
-        String timeStamp = "null";
-        if (row[3] != null) {
-          timeStamp = row[3].toString();
-        }
-        Long line = parsingContext.currentLine();
-
-        try {
-          outputHandler.writeLine(queryString, validity, userAgent, timeStamp, line, day, inputFile);
-        } catch (NullPointerException e) {
-          outputHandler.writeLine("", QueryHandler.Validity.INTERNAL_ERROR, userAgent, timeStamp, line, day, inputFile);
-          logger.error("Unexpected Null Pointer Exception in writeLine.", e);
-        }
+    for (CSVRecord csvRecord : csvParser) {
+      if (csvRecord.size() <= 1) {
+        logger.warn("Ignoring line without tab while parsing.");
+        return;
       }
-    };
+      Long line = csvRecord.getRecordNumber();
+      Tuple2<String, QueryHandler.Validity> queryTuple = decode(csvRecord.get("uri_query").toString(), inputFile, line);
 
-    parserSettings.setProcessor(rowProcessor);
-
-    TsvParser parser = new TsvParser(parserSettings);
-
-    parser.parse(reader);
+      String queryString = queryTuple._1;
+      QueryHandler.Validity validity = queryTuple._2;
+      String userAgent = "null";
+      String possibleUserAgent = csvRecord.get("user_agent");
+      if (possibleUserAgent != null)
+        userAgent = possibleUserAgent;
+      String timeStamp = "null";
+      String possibleTimeStamp = csvRecord.get("ts");
+      if (possibleTimeStamp != null)
+        timeStamp = possibleTimeStamp;
+      try {
+        outputHandler.writeLine(queryString, validity, userAgent, timeStamp, line, day, inputFile);
+      } catch (NullPointerException e) {
+        outputHandler.writeLine("", QueryHandler.Validity.INTERNAL_ERROR, userAgent, timeStamp, line, day, inputFile);
+        logger.error("Unexpected Null Pointer Exception in writeLine.", e);
+      }
+    }
 
     outputHandler.closeFiles();
-
-    if (preprocessedWriter != null) {
-      preprocessedWriter.close();
-    }
   }
 }
